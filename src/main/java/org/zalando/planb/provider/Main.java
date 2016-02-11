20d7183f234f0703d6f5f3445e2bb90c375b79ce
Joerg Bellmann <joerg.bellmann@zalando.de>
2016-02-11 19:14:12 +0100

    fix imports

--- src/main/java/org/zalando/planb/provider/Main.java
+++ src/main/java/org/zalando/planb/provider/Main.java
@@ -1,14 +1,11 @@
 package org.zalando.planb.provider;
 
-import com.fasterxml.jackson.databind.JsonSerializer;
-import com.fasterxml.jackson.databind.Module;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.databind.module.SimpleModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
 
 @SpringBootApplication
 public class Main {
