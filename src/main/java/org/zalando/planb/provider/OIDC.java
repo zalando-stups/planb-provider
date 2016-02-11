4cac08bee68d045f79b91721b1431f1ea9329ee9
Joerg Bellmann <joerg.bellmann@zalando.de>
2016-02-11 11:58:25 +0100

    first-step to integration-tests

--- src/main/java/org/zalando/planb/provider/OIDC.java
+++ src/main/java/org/zalando/planb/provider/OIDC.java
@@ -1,15 +1,12 @@
 package org.zalando.planb.provider;
 
-import org.springframework.boot.*;
-import org.springframework.boot.autoconfigure.*;
-import org.springframework.stereotype.*;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class OIDC {
 
     @RequestMapping("/")
-    @ResponseBody
     String home() {
         return "Hello World!";
     }
