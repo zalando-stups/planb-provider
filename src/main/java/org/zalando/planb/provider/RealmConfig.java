7d1ea97335362ffa71ad49ecaab7ed6d08748d35
Tobias Sarnowski <tobias.sarnowski@zalando.de>
2016-02-12 16:31:42 +0100

    added customer realm to official realm list

--- src/main/java/org/zalando/planb/provider/RealmConfig.java
+++ src/main/java/org/zalando/planb/provider/RealmConfig.java
@@ -1,5 +1,6 @@
 package org.zalando.planb.provider;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
@@ -10,13 +11,19 @@ import java.util.Map;
 
 @Component
 public class RealmConfig {
-
     private final Map<String,Realm> realms = new HashMap<>();
+
+    @Autowired
+    private CustomerLoginRealm customerLoginRealm;
+
+    @Autowired
+    private TestRealm testRealm;
 
     @PostConstruct
     void setup() {
         // find a nicer way to initialize as beans and provide configuration
-        realms.put("/test", new TestRealm());
+        realms.put("/test", testRealm);
+        realms.put("/customers", customerLoginRealm);
     }
 
     Realm get(String name) {
