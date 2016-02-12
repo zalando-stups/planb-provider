cba01391f3e0bb4d505d35b418c1a5eab3900185
Joerg Bellmann <joerg.bellmann@zalando.de>
2016-02-12 13:02:20 +0100

    you have to put @PathVariable, @RequestBody .. in the signature, sorry

--- src/main/java/org/zalando/planb/provider/ClientsApiController.java
+++ src/main/java/org/zalando/planb/provider/ClientsApiController.java
@@ -1,13 +1,15 @@
 package org.zalando.planb.provider;
 
+import static org.slf4j.LoggerFactory.getLogger;
+import static org.springframework.http.HttpStatus.OK;
+
 import org.slf4j.Logger;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.zalando.planb.provider.api.Client;
 import org.zalando.planb.provider.api.ClientsApi;
-
-import static org.slf4j.LoggerFactory.getLogger;
-import static org.springframework.http.HttpStatus.OK;
 
 @RestController
 public class ClientsApiController implements ClientsApi {
@@ -15,7 +17,8 @@ public class ClientsApiController implements ClientsApi {
     private final Logger log = getLogger(getClass());
 
     @Override
-    public ResponseEntity<Void> clientsRealmIdPut(String realm, String id, Client client) {
+    public ResponseEntity<Void> clientsRealmIdPut(@PathVariable String realm, @PathVariable String id,
+            @RequestBody Client client) {
         log.info("Create or replace client /{}/{}: {}", realm, id, client);
         return new ResponseEntity<>(OK);
     }
