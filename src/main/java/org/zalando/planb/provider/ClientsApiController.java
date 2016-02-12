e9f106ca62c25e413785de094036bc20b8943110
Andre Hartmann <andre.hartmann@zalando.de>
2016-02-12 13:30:56 +0100

    #2 fix ClientsApiController by adding parameter annotations

--- src/main/java/org/zalando/planb/provider/ClientsApiController.java
+++ src/main/java/org/zalando/planb/provider/ClientsApiController.java
@@ -1,8 +1,5 @@
 package org.zalando.planb.provider;
 
-import static org.slf4j.LoggerFactory.getLogger;
-import static org.springframework.http.HttpStatus.OK;
-
 import org.slf4j.Logger;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,26 +8,36 @@ import org.springframework.web.bind.annotation.RestController;
 import org.zalando.planb.provider.api.Client;
 import org.zalando.planb.provider.api.ClientsApi;
 
+import static org.slf4j.LoggerFactory.getLogger;
+import static org.springframework.http.HttpStatus.OK;
+
 @RestController
 public class ClientsApiController implements ClientsApi {
 
     private final Logger log = getLogger(getClass());
 
     @Override
-    public ResponseEntity<Void> clientsRealmIdPut(@PathVariable String realm, @PathVariable String id,
+    public ResponseEntity<Void> clientsRealmIdPut(
+            @PathVariable("realm") String realm,
+            @PathVariable("id") String id,
             @RequestBody Client client) {
         log.info("Create or replace client /{}/{}: {}", realm, id, client);
         return new ResponseEntity<>(OK);
     }
 
     @Override
-    public ResponseEntity<Void> clientsRealmIdDelete(String realm, String id) {
+    public ResponseEntity<Void> clientsRealmIdDelete(
+            @PathVariable("realm") String realm,
+            @PathVariable("id") String id) {
         log.info("Delete client /{}/{}", realm, id);
         return new ResponseEntity<>(OK);
     }
 
     @Override
-    public ResponseEntity<Void> clientsRealmIdPatch(String realm, String id, Client client) {
+    public ResponseEntity<Void> clientsRealmIdPatch(
+            @PathVariable("realm") String realm,
+            @PathVariable("id") String id,
+            @RequestBody Client client) {
         log.info("Update client /{}/{}: {}", realm, id, client);
         return new ResponseEntity<>(OK);
     }
