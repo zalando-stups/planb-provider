d5eeceb9aeccf016831bffc1e5fd57b0f71340f9
Joerg Bellmann <joerg.bellmann@zalando.de>
2016-02-12 17:21:46 +0100

    implementations now package-protected, so use 'getClass().getName()'
    to verify implementation class

--- src/test/java/org/zalando/planb/provider/RealmPluginTest.java
+++ src/test/java/org/zalando/planb/provider/RealmPluginTest.java
@@ -26,46 +26,44 @@ public class RealmPluginTest extends AbstractSpringTest {
         // Cassandra-Realms
         RealmPlugin cassandraRealmPlugin_1 = realmPluginRegistry.getPluginFor("brot");
         Assertions.assertThat(cassandraRealmPlugin_1).isNotNull();
-        // Assertions.assertThat(cassandraRealmPlugin_1).isInstanceOf(CassandraRealmPlugin.class);
+        Assertions.assertThat(cassandraRealmPlugin_1.getClass().getName()).contains("CassandraRealmPlugin");
         Realm cassandraRealm_1 = cassandraRealmPlugin_1.get("brot");
         Assertions.assertThat(cassandraRealm_1).isInstanceOf(Realm.class);
-        // Assertions.assertThat(cassandraRealm_1).isInstanceOf(CassandraRealm.class);
+        Assertions.assertThat(cassandraRealm_1.getClass().getSimpleName()).contains("CassandraRealm");
 
         RealmPlugin cassandraRealmPlugin_2 = realmPluginRegistry.getPluginFor("zwiebel");
         Assertions.assertThat(cassandraRealmPlugin_2).isNotNull();
-        // Assertions.assertThat(cassandraRealmPlugin_2).isInstanceOf(CassandraRealmPlugin.class);
         Realm cassandraRealm_2 = cassandraRealmPlugin_2.get("zwiebel");
         Assertions.assertThat(cassandraRealm_2).isInstanceOf(Realm.class);
-        // Assertions.assertThat(cassandraRealm_2).isInstanceOf(CassandraRealm.class);
+        Assertions.assertThat(cassandraRealm_2.getClass().getSimpleName()).contains("CassandraRealm");
 
         Assertions.assertThat(cassandraRealm_1).isNotEqualTo(cassandraRealm_2);
 
         Realm cassandraRealm_3 = realmConfig.get("brot");
         Assertions.assertThat(cassandraRealm_3).isInstanceOf(Realm.class);
-        // Assertions.assertThat(cassandraRealm_3).isInstanceOf(CassandraRealm.class);
+        Assertions.assertThat(cassandraRealm_3.getClass().getSimpleName()).contains("CassandraRealm");
 
         Assertions.assertThat(cassandraRealm_3).isEqualTo(cassandraRealm_1);
 
         // Soap-Realms
         RealmPlugin soapRealmPlugin_1 = realmPluginRegistry.getPluginFor("zoap_1");
         Assertions.assertThat(soapRealmPlugin_1).isNotNull();
-        // Assertions.assertThat(soapRealmPlugin_1).isInstanceOf(SoapRealmPlugin.class);
+        Assertions.assertThat(soapRealmPlugin_1.getClass().getName()).contains("SoapRealmPlugin");
         Realm soapRealm_1 = soapRealmPlugin_1.get("zoap_1");
         Assertions.assertThat(soapRealm_1).isInstanceOf(Realm.class);
-        // Assertions.assertThat(soapRealm_1).isInstanceOf(SoapRealm.class);
+        Assertions.assertThat(soapRealm_1.getClass().getName()).contains("SoapRealm");
 
         RealmPlugin soapRealmPlugin_2 = realmPluginRegistry.getPluginFor("zoap_2");
         Assertions.assertThat(soapRealmPlugin_2).isNotNull();
-        // Assertions.assertThat(soapRealmPlugin_2).isInstanceOf(SoapRealmPlugin.class);
+
         Realm soapRealm_2 = soapRealmPlugin_2.get("zoap_2");
         Assertions.assertThat(soapRealm_2).isInstanceOf(Realm.class);
-        // Assertions.assertThat(soapRealm_2).isInstanceOf(SoapRealm.class);
 
         Assertions.assertThat(soapRealm_1).isNotEqualTo(soapRealm_2);
 
         Realm soapRealm_3 = realmConfig.get("zoap_2");
         Assertions.assertThat(soapRealm_3).isInstanceOf(Realm.class);
-        // Assertions.assertThat(soapRealm_3).isInstanceOf(SoapRealm.class);
+        Assertions.assertThat(soapRealm_3.getClass().getName()).contains("SoapRealm");
 
         Assertions.assertThat(soapRealm_3).isEqualTo(soapRealm_2);
 
@@ -73,7 +71,6 @@ public class RealmPluginTest extends AbstractSpringTest {
         RealmPlugin defaultRealmPlugin = realmPluginRegistry.getPluginFor("notExistent_takeDefault",
                 cassandraRealmPlugin_2);
         Assertions.assertThat(defaultRealmPlugin).isNotNull();
-        // Assertions.assertThat(defaultRealmPlugin).isInstanceOf(CassandraRealmPlugin.class);
         Realm defaultRealm = defaultRealmPlugin.get("zwiebel");
         Assertions.assertThat(defaultRealm).isEqualTo(cassandraRealm_2);
     }
