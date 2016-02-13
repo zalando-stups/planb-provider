c82676fae7e3220464980d52e352a6bda5bff409
Andre Hartmann <andre.hartmann@zalando.de>
2016-02-13 21:41:57 +0100

    small improvements to the spring context and the maven build

--- src/main/java/org/zalando/planb/provider/Main.java
+++ src/main/java/org/zalando/planb/provider/Main.java
@@ -1,21 +1,25 @@
 package org.zalando.planb.provider;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.Module;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.cassandra.CassandraAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration(exclude = {
+        CassandraAutoConfiguration.class // TODO Disabled for now, because we have our own. Let's figure out, if Spring's config would also fit for us
+})
 @ComponentScan // for IntelliJ
 public class Main {
 
     @Bean
-    ObjectMapper createObjectMapper() {
-        ObjectMapper om = new ObjectMapper();
-        om.registerModule(new SimpleModule().addSerializer(OIDCSigningKeysResponse.class, new OIDCSigningKeysSerializer()));
-        return om;
+    Module oidcSigningKeysResponseModule() {
+        return new SimpleModule().addSerializer(OIDCSigningKeysResponse.class, new OIDCSigningKeysSerializer());
     }
 
     public static void main(String[] args) throws Exception {
