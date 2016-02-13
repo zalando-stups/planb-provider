81f654d92b0c742a59663609eddc7445466b21b9
Andre Hartmann <andre.hartmann@zalando.de>
2016-02-13 20:55:39 +0100

    remove unused code

--- src/main/java/org/zalando/planb/provider/RealmAuthenticationException.java
+++ src/main/java/org/zalando/planb/provider/RealmAuthenticationException.java
@@ -5,10 +5,6 @@ package org.zalando.planb.provider;
  */
 public class RealmAuthenticationException extends Exception {
 
-    public RealmAuthenticationException() {
-        super();
-    }
-
     public RealmAuthenticationException(String message) {
         super(message);
     }
--- src/main/java/org/zalando/planb/provider/RealmAuthorizationException.java
+++ src/main/java/org/zalando/planb/provider/RealmAuthorizationException.java
@@ -5,10 +5,6 @@ package org.zalando.planb.provider;
  */
 public class RealmAuthorizationException extends Exception {
 
-    public RealmAuthorizationException() {
-        super();
-    }
-
     public RealmAuthorizationException(String message) {
         super(message);
     }
