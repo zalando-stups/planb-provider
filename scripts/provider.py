fbfd94c7c56ca12c7407bd2bf0b1a94c8e5a487c
Henning Jacobs <henning@jacobs1.de>
2016-02-16 16:48:32 +0100

    call to create user

--- scripts/provider.py
+++ scripts/provider.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
+'''
+CLI client for the Plan B Open ID Provider REST API
+
+TODO: migrate to use https://github.com/zalando/openapi-cli-client
+'''
 
 import base64
 import bcrypt
@@ -19,6 +24,11 @@ def clients():
     pass
 
 
+@cli.group(cls=AliasedGroup)
+def users():
+    pass
+
+
 def get_b64hash(secret):
     bcrypt_hash = bcrypt.hashpw(secret.encode('utf-8'), bcrypt.gensalt())
     return base64.b64encode(bcrypt_hash).decode('utf-8')
@@ -30,12 +40,33 @@ def get_b64hash(secret):
 @click.option('--password', prompt=True, hide_input=True)
 @click.option('--non-confidential', is_flag=True, default=False)
 def create_client(realm_client_id, url, password, non_confidential):
+    '''
+    Create/update client
+    '''
     if not url:
         raise click.UsageError('Missing URL (--url)')
     data = {'secret_hash': get_b64hash(password),
             'is_confidential': not non_confidential}
     with Action('Creating client {}..'.format(realm_client_id)):
         r = requests.put(url + '/clients/{}'.format(realm_client_id.lstrip('/')), data=json.dumps(data),
+                         headers={'Content-Type': 'application/json'})
+        print(r.text)
+        r.raise_for_status()
+
+
+@users.command('create')
+@click.argument('realm_username')
+@click.option('--url')
+@click.option('--password', prompt=True, hide_input=True)
+def create_user(realm_username, url, password):
+    '''
+    Create/update user
+    '''
+    if not url:
+        raise click.UsageError('Missing URL (--url)')
+    data = {'password_hashes': [get_b64hash(password)]}
+    with Action('Creating user {}..'.format(realm_username)):
+        r = requests.put(url + '/users/{}'.format(realm_username.lstrip('/')), data=json.dumps(data),
                          headers={'Content-Type': 'application/json'})
         print(r.text)
         r.raise_for_status()
