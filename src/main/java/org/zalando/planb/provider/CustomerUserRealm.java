77f0fbac31abef8e617ce2a4bf171e74b9fe8016
Michele Randi <michele.randi@gmail.com>
2016-02-17 17:11:22 +0100

    refactoring

--- src/main/java/org/zalando/planb/provider/CustomerUserRealm.java
+++ src/main/java/org/zalando/planb/provider/CustomerUserRealm.java
@@ -1,16 +1,9 @@
 package org.zalando.planb.provider;
 
-import org.apache.cxf.interceptor.LoggingInInterceptor;
-import org.apache.cxf.interceptor.LoggingOutInterceptor;
-import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
-import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
-import org.zalando.stups.oauth2.jaxws.cxf.interceptors.OAuth2TokenInterceptor;
-import org.zalando.stups.tokens.AccessTokens;
 
-import javax.annotation.PostConstruct;
 import java.util.Map;
 
 import static java.util.Collections.singletonMap;
@@ -21,43 +14,15 @@ import static java.util.Optional.ofNullable;
 public class CustomerUserRealm implements UserRealm {
 
     public static final int APP_DOMAIN_ID = 1;
-    public static final String SERVICE_ID = "customerLogin";
     public static final String SUCCESS_STATUS = "SUCCESS";
     public static final String UID = "uid";
 
-    private final Environment environment;
-    private final AccessTokens accessTokens;
-    private final CustomerRealmProperties customerRealmProperties;
+    private CustomerRealmWebService customerRealmWebService;
     private String realmName;
 
     @Autowired
-    public CustomerUserRealm(Environment environment, AccessTokens accessTokens, CustomerRealmProperties customerRealmProperties) {
-        this.environment = environment;
-        this.accessTokens = accessTokens;
-        this.customerRealmProperties = customerRealmProperties;
-    }
-
-    private CustomerRealmWebService customerRealmWebService;
-
-    @PostConstruct
-    void initialize() {
-        JaxWsProxyFactoryBean factory = new JaxWsProxyFactoryBean();
-        factory.setAddress(customerRealmProperties.getServiceUrl());
-        factory.setServiceClass(CustomerRealmWebService.class);
-        OAuth2TokenInterceptor oAuth2TokenInterceptor = new OAuth2TokenInterceptor(accessTokens, SERVICE_ID);
-        factory.getOutInterceptors().add(oAuth2TokenInterceptor);
-
-        if (environment.containsProperty("debug") || environment.containsProperty("trace")) {
-            LoggingInInterceptor loggingInInterceptor = new LoggingInInterceptor();
-            loggingInInterceptor.setPrettyLogging(true);
-            LoggingOutInterceptor loggingOutInterceptor = new LoggingOutInterceptor();
-            loggingOutInterceptor.setPrettyLogging(true);
-            factory.getInInterceptors().add(loggingInInterceptor);
-            factory.getOutInterceptors().add(loggingOutInterceptor);
-        }
-
-        customerRealmWebService = (CustomerRealmWebService) factory.create();
-
+    public CustomerUserRealm(CustomerRealmWebService customerRealmWebService) {
+        this.customerRealmWebService = customerRealmWebService;
     }
 
     @Override
--- src/main/java/org/zalando/planb/provider/Main.java
+++ src/main/java/org/zalando/planb/provider/Main.java
@@ -4,7 +4,6 @@ import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.cassandra.CassandraAutoConfiguration;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -17,7 +16,6 @@ import java.security.Security;
         CassandraAutoConfiguration.class // TODO Disabled for now, because we have our own. Let's figure out, if Spring's config would also fit for us
 })
 @ComponentScan // for IntelliJ
-@EnableConfigurationProperties(CustomerRealmProperties.class)
 @EnableHystrix
 @EnableAspectJAutoProxy
 public class Main {
