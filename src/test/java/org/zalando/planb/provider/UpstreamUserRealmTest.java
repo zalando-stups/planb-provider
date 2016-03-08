9ad169255f3755c653eab0f0294c2b981cf03ad4
Henning Jacobs <henning@jacobs1.de>
2016-03-08 12:22:39 +0100

    #51 test UpstreamUserRealm

--- src/test/java/org/zalando/planb/provider/UpstreamUserRealmTest.java
+++ src/test/java/org/zalando/planb/provider/UpstreamUserRealmTest.java
@@ -12,6 +12,7 @@ import java.util.Map;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.StrictAssertions.fail;
 
 /**
  * Created by hjacobs on 07.03.16.
@@ -28,10 +29,24 @@ public class UpstreamUserRealmTest {
         stubFor(get(urlPathEqualTo("/token")).willReturn(aResponse()
                 .withStatus(200)
                 .withBody("mytok\n")));
+        stubFor(get(urlPathEqualTo("/token-invalid")).willReturn(aResponse()
+                .withStatus(401)
+                .withBody("invalid\n")));
         stubFor(get(urlPathEqualTo("/tokeninfo")).willReturn(aResponse()
                 .withStatus(200)
                 .withHeader("Content-Type", "application/json")
                 .withBody("{\"uid\":\"myuid\"}")));
+        stubFor(get(urlPathEqualTo("/tokeninfo-invalid")).willReturn(aResponse()
+                .withStatus(401)));
+    }
+
+    @Test
+    public void testInitialize() {
+        UpstreamRealmProperties props = new UpstreamRealmProperties();
+        UpstreamUserRealm realm = new UpstreamUserRealm(props);
+        realm.initialize("myrealm");
+        assertThat(realm.getName()).isEqualTo("myrealm");
+        assertThat(realm.maskSubject("mysub")).isEqualTo("mysub");
     }
 
     @Test
@@ -63,5 +78,39 @@ public class UpstreamUserRealmTest {
 
         Map<String, String> response = realm.authenticate("myuser", "mypass", ImmutableSet.of("myscope"), ImmutableSet.of("myscope"));
         assertThat(response).containsEntry("sub", "myuid");
+    }
+
+    @Test
+    public void testAuthenticateWrongCredentials() {
+        UpstreamRealmProperties props = new UpstreamRealmProperties();
+        props.setTokenServiceUrl("http://localhost:" + wiremockPort + "/token-invalid");
+        props.setTokenInfoUrl("http://localhost:" + wiremockPort + "/tokeninfo");
+        UpstreamUserRealm realm = new UpstreamUserRealm(props);
+
+        try {
+            realm.authenticate("myuser", "mypass", ImmutableSet.of("myscope"), ImmutableSet.of("myscope"));
+            fail("Authenticate should have thrown exception");
+        } catch (UserRealmAuthenticationException ex) {
+            assertThat(ex.getErrorType()).isEqualTo("invalid_grant");
+            assertThat(ex.getErrorLocation()).contains(RestException.USER_ERROR);
+            assertThat(ex.getMessage()).isEqualTo("User myuser login failed");
+        }
+    }
+
+    @Test
+    public void testAuthenticateTokenInfoFails() {
+        UpstreamRealmProperties props = new UpstreamRealmProperties();
+        props.setTokenServiceUrl("http://localhost:" + wiremockPort + "/token");
+        props.setTokenInfoUrl("http://localhost:" + wiremockPort + "/tokeninfo-invalid");
+        UpstreamUserRealm realm = new UpstreamUserRealm(props);
+
+        try {
+            realm.authenticate("myuser", "mypass", ImmutableSet.of("myscope"), ImmutableSet.of("myscope"));
+            fail("Authenticate should have thrown exception");
+        } catch (UserRealmAuthenticationException ex) {
+            assertThat(ex.getErrorType()).isEqualTo("invalid_grant");
+            assertThat(ex.getErrorLocation()).contains(RestException.USER_ERROR);
+            assertThat(ex.getMessage()).isEqualTo("User myuser login failed");
+        }
     }
 }
