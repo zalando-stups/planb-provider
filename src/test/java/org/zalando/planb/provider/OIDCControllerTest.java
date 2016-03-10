290ff2be1b3cc572ee800e69a3ad73ec9d77123b
Henning Jacobs <henning@jacobs1.de>
2016-03-10 20:14:21 +0100

    add trivial test

--- src/test/java/org/zalando/planb/provider/OIDCControllerTest.java
+++ src/test/java/org/zalando/planb/provider/OIDCControllerTest.java
@@ -1,6 +1,14 @@
 package org.zalando.planb.provider;
 
+import org.junit.Test;
+import java.util.Optional;
 
 public class OIDCControllerTest {
+
+    @Test(expected = RealmNotFoundException.class)
+    public void realmNotFound() {
+        RealmConfig realms = new RealmConfig();
+        OIDCController.getRealmName(realms, Optional.empty(), Optional.of("some.header.value"));
+    }
 
 }
