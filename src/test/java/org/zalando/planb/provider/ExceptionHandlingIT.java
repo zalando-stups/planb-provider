95fabb4d9a4cf96b7a58497a5cbc316b8ef2fb07
Daniel del Hoyo <daniel.del.hoyo@zalando.de>
2016-04-13 10:40:40 +0200

    #98: Do not log http client mistakes on error level - Handle HttpRequestMethodNotSupportedExpection - Added test case

--- src/test/java/org/zalando/planb/provider/ExceptionHandlingIT.java
+++ src/test/java/org/zalando/planb/provider/ExceptionHandlingIT.java
@@ -1,22 +1,34 @@
 package org.zalando.planb.provider;
 
-import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @ActiveProfiles("it")
 public class ExceptionHandlingIT extends AbstractOauthTest {
 
     @Test
-    public void error() {
+    public void runtimeException() {
         RequestEntity request = RequestEntity.get(getUriWithPathAsUri("/throwError")).build();
         getRestTemplate().setErrorHandler(new PassThroughResponseErrorHandler());
         ResponseEntity<String> response = getRestTemplate().exchange(request, String.class);
-        Assertions.assertThat(response.getStatusCode().is5xxServerError()).isTrue();
-        Assertions.assertThat(response.getBody()).contains("TEST_ERROR");
-        Assertions.assertThat(response.getBody()).doesNotContain("Caused by");
+        assertThat(response.getStatusCode().is5xxServerError()).isTrue();
+        assertThat(response.getBody()).contains("TEST_ERROR");
+        assertThat(response.getBody()).doesNotContain("Caused by");
+    }
+
+    @Test
+    public void methodNotAllowedException() {
+        RequestEntity request = RequestEntity.get(getUriWithPathAsUri("/methodNotAllowed")).build();
+        getRestTemplate().setErrorHandler(new PassThroughResponseErrorHandler());
+        ResponseEntity<String> response = getRestTemplate().exchange(request, String.class);
+        assertThat(response.getStatusCode().is4xxClientError()).isTrue();
+        assertThat(response.getBody()).contains("not supported");
+        assertThat(response.getHeaders().get("Allow")).isNotEmpty();
+        assertThat(response.getBody()).doesNotContain("Caused by");
     }
 
 }
