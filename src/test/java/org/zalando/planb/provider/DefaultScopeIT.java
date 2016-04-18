e46919e9702c18a34bee98d9e5f5dd238890a4ff
Victor Roldan Betancort <victor.roldan.betancort@zalando.de>
2016-04-18 14:17:04 +0200

    Issue #113: Default scopes for service clients does not work via resource owner password grant.
    - Added integration test that demonstrates default scopes for resource owner password grant
    - Fixed issue on OIDCController, improper client credentials where being passed to ScopeService

--- src/test/java/org/zalando/planb/provider/DefaultScopeIT.java
+++ src/test/java/org/zalando/planb/provider/DefaultScopeIT.java
@@ -8,7 +8,11 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 
+import java.util.Base64;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.http.RequestEntity.post;
 
 @ActiveProfiles("it")
 public class DefaultScopeIT extends AbstractOauthTest {
@@ -30,6 +34,26 @@ public class DefaultScopeIT extends AbstractOauthTest {
 
         ResponseEntity<AuthorizeResponse> loginResponse = getRestTemplate().exchange(request, AuthorizeResponse.class);
         assertThat(loginResponse.getBody().getScopes()).containsExactly("uid");
+    }
+
+    @Test
+    public void clientSpecificDefaultsResourceOwnerPasswordGrantTest() {
+        MultiValueMap<String, Object> requestParameters = new LinkedMultiValueMap<>();
+        requestParameters.add("grant_type", "password");
+        requestParameters.add("realm", "/services");
+        requestParameters.add("username", "testuser");
+        requestParameters.add("password", "test");
+
+        String basicAuth = Base64.getEncoder().encodeToString(("testdefaultscope" + ':' + "test").getBytes(UTF_8));
+
+        RequestEntity<MultiValueMap<String, Object>> request =
+                post(getAccessTokenUri())
+                        .header("Authorization", "Basic " + basicAuth)
+                        .body(requestParameters);
+
+        ResponseEntity<OIDCCreateTokenResponse> loginResponse = getRestTemplate().exchange(request, OIDCCreateTokenResponse.class);
+        assertThat(loginResponse.getBody().getRealm()).isEqualTo("/services");
+        assertThat(loginResponse.getBody().getScope()).isEqualTo("uid");
     }
 
     @Test
